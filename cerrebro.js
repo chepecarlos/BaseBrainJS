f5a19c9e4853f14fbc672f9d2cc065f447d1c998
chepecarlos <chepecarlos@alswblog.org>
2018-05-05 10:13:40 -0600

    Completando el paquete necesario

--- cerrebro.js
+++ cerrebro.js
@@ -1,6 +1,7 @@
+
 window.onload = function() {
-  var Entrada = document.querySelector("input")
-  var Spoiler = document.querySelector("#spoiler")
+  const Entrada = document.querySelector("input")
+  const Spoiler = document.querySelector("#spoiler")
 
   Entrada.addEventListener("change", (e) =>
     Spoiler.style.background = e.target.value
