3af1d06818c41333362e6f64e2cff9041a40b089
chepecarlos <chepecarlos@alswblog.org>
2018-05-08 13:41:40 -0600

    Codigo RGB a exa

--- cerrebro.js
+++ cerrebro.js
@@ -1,9 +1,26 @@
-
 window.onload = function() {
   const Entrada = document.querySelector("input")
   const Spoiler = document.querySelector("#spoiler")
 
-  Entrada.addEventListener("change", (e) =>
-    Spoiler.style.background = e.target.value
-  )
+  Entrada.addEventListener("change", (e) => {
+    Spoiler.style.background = e.target.value;
+    console.log("Cambiando Color");
+  })
+
+
+
+  function getRgb(hex) {
+    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+      return r + r + g + g + b + b;
+    });
+
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+      r: Math.round(parseInt(result[1], 16) / 2.55) / 100,
+      g: Math.round(parseInt(result[2], 16) / 2.55) / 100,
+      b: Math.round(parseInt(result[3], 16) / 2.55) / 100,
+    } : null;
+  }
+
 }
